ea1abf7168e2b4aaf8a45028ce397db5a5e4178f
ari oren <arioren100@gmail.com>
2024-12-19 10:23:40 +0200

    Endpoints that give the most dangerous regions

--- app/repository/terror_data.py
+++ app/repository/terror_data.py
@@ -4,7 +4,7 @@
 # the calculation. a. Filter option by -5Top or All
 from sqlalchemy import func
 from app.db.database import session_maker
-from app.db.model import Event, Casualties, AttackType
+from app.db.model import Event, Casualties, AttackType, Region, Location
 
 
 def deadliest_attack_types(filter_option="Top 5"):
@@ -27,3 +27,27 @@ def deadliest_attack_types(filter_option="Top 5"):
         result = query.all()
 
         return [{"name": row[1], "score": row[2]} for row in result]
+
+
+
+def average_casualties_by_region(filter_option="Top 5"):
+    with session_maker() as session:
+        query = (
+            session.query(
+                Region.id,
+                Region.name,
+                func.avg(Casualties.killed * 2 + Casualties.wounded).label("average_casualties")
+            )
+            .join(Location, Region.id == Location.region_id)
+            .join(Event, Location.id == Event.location_id)
+            .join(Casualties)
+            .group_by(Region.id, Region.name)
+            .order_by(func.avg(Casualties.killed * 2 + Casualties.wounded).desc())
+        )
+
+        if filter_option == "Top 5":
+            query = query.limit(5)
+
+        result = query.all()
+
+        return [{"region": row[1], "average_casualties": float(row[2])} for row in result]
--- app/routs/queries.py
+++ app/routs/queries.py
@@ -4,9 +4,6 @@ import app.repository.terror_data as repo
 
 terror_bluprint = Blueprint('terror', __name__)
 
-# 1. The deadliest attack types. "Deadliest" = the types with the highest number of casualties,
-# killed and injured, where a casualty = 1 point and a kill is worth 2 points for the calculation.
-# a. Filter option by -5Top or All
 @terror_bluprint.route('/deadliest_attack_types', methods=['GET'])
 def get_deadliest_attack_types():
     res = repo.deadliest_attack_types("All")
@@ -15,4 +12,14 @@ def get_deadliest_attack_types():
 @terror_bluprint.route('/deadliest_attack_types/top5', methods=['GET'])
 def get_deadliest_attack_types():
     res = repo.deadliest_attack_types()
+    return jsonify(res)
+
+@terror_bluprint.route('/average_casualties_by_region', methods=['GET'])
+def get_average_casualties_by_region():
+    res = repo.average_casualties_by_region("All")
+    return jsonify(res)
+
+@terror_bluprint.route('/average_casualties_by_region/top5', methods=['GET'])
+def get_average_casualties_by_region():
+    res = repo.average_casualties_by_region("Top 5")
     return jsonify(res)
